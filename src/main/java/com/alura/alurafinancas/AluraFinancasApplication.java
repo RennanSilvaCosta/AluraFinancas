95e83645dee86a542cf5b23c85aa014754773bd3
Rennan Costa <rennansilvacosta@hotmail.com>
2022-01-19 16:40:31 -0300

    estrutura base

---
+++ src/main/java/com/alura/alurafinancas/AluraFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.alura.alurafinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraFinancasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraFinancasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alura/alurafinancas/AluraFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.alurafinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraFinancasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
